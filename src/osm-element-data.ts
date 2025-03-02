26fb9ea6cbed582dd5b3fbd73e5681bcaa896348
Anton Khorev <tony29@yandex.ru>
2025-03-02 10:20:39 +0300

    Parse element versions with 'history'

--- src/osm-element-data.ts
+++ src/osm-element-data.ts
@@ -13,8 +13,13 @@ export type OsmElementVersionData = {
 	version: number
 }
 
+const osmElementVersionRegExp=new RegExp(`^(node|way|relation)/(\\d+)/(?:history/)?(\\d+)$`)
+
 export function getOsmElementVersionDataFromString(s: string): OsmElementVersionData {
-	const [type,idString,versionString]=s.split('/')
+	const match=s.match(osmElementVersionRegExp)
+	if (!match) throw new TypeError(`Received invalid element reference`)
+
+	const [,type,idString,versionString]=match
 	if (!isOsmElementType(type)) throw new TypeError(`Received invalid element type`)
 	const id=toPositiveInteger(idString)
 	const version=toPositiveInteger(versionString)
--- test/osm-element-data.test.ts
+++ test/osm-element-data.test.ts
@@ -1,13 +1,33 @@
 import { getOsmElementVersionDataFromString } from '../src/osm-element-data'
 
-test(`parses regular type/id/version`,()=>{
-	expect(
-		getOsmElementVersionDataFromString(
-			`node/23/12`
-		)
-	).toStrictEqual({
-		type: 'node',
-		id: 23,
-		version: 12
+describe(`getOsmElementVersionDataFromString`,()=>{
+	test(`fails on empty input`,()=>{
+		expect(
+			()=>getOsmElementVersionDataFromString(
+				``
+			)
+		).toThrow(TypeError)
+	})
+	test(`parses :type/:id/:version`,()=>{
+		expect(
+			getOsmElementVersionDataFromString(
+				`node/23/12`
+			)
+		).toStrictEqual({
+			type: 'node',
+			id: 23,
+			version: 12
+		})
+	})
+	test(`parses :type/:id/history/:version`,()=>{
+		expect(
+			getOsmElementVersionDataFromString(
+				`way/221/history/2`
+			)
+		).toStrictEqual({
+			type: 'way',
+			id: 221,
+			version: 2
+		})
 	})
 })
