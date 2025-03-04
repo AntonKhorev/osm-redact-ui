851494e32546be8a164d4496ad85be2aaa900ad5
Anton Khorev <tony29@yandex.ru>
2025-03-04 13:39:20 +0300

    Add "How to use"

--- src/index.ts
+++ src/index.ts
@@ -26,12 +26,51 @@ function main(): void {
 	const elementsStage=new ElementsStage(osmAuthManager.currentProvider)
 	const changesetStage=new ChangesetStage(osmAuthManager.currentProvider,elementsStage)
 
+	const em=makeElement('em')()
+
 	document.body.append(
 		makeElement('main')()(
 			makeElement('h1')()(`Changeset redaction UI`),
 			makeDiv('source-code')(
 				makeElement('small')()(
 					makeLink(`source code`,`https://github.com/AntonKhorev/osm-redact-ui`)
+				)
+			),
+			makeElement('details')()(
+				makeElement('summary')()(
+					`How to use`
+				),
+				makeElement('ol')()(
+					makeElement('li')()(
+						`Find changesets with offending data`
+					),
+					makeElement('li')()(
+						`Before using this tool, revert the changesets or delete the offending data by any other means`
+					),
+					makeElement('li')()(
+						`In this tool, enter the changesets into `,em(`Changeset ids or URLs to redact`)
+					),
+					makeElement('li')()(
+						`Click `,em(`Fetch target elements`)
+					),
+					makeElement('li')()(
+						`If there are no errors, review the changesets in the `,em(`Scanned changesets`),` table`
+					),
+					makeElement('li')()(
+						`Review the element versions to be redacted in the `,em(`Target elements`),` section`
+					),
+					makeElement('li')()(
+						`Enter `,em(`Redaction id or URL`),`, possibly by visiting the redactions page using the link below the input`
+					),
+					makeElement('li')()(
+						`Click `,em(`Redact targets`)
+					)
+				),
+				makeElement('p')()(
+					`This workflow redacts every target element version up to (but not including) the current version and may not be suitable for elements that had the offending data removed long ago. `+
+					`Such elements are likely to have clean versions after the removal. `+
+					`These versions don't need to be redacted, but this tool doesn't have the capabilities to detect that at the moment. `+
+					`You can try removing clean versions from `,em(`Element versions to redact`),` manually.`
 				)
 			),
 			authStage.$section,
